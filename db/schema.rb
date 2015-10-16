b9dd0cd396c2693369e2869a5bfa72d3eaff71de
Vaughn Dice <donvice@gmail.com>
2015-10-16 10:33:19 -0700

    bring in rspec and add create tags migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016171627) do
+ActiveRecord::Schema.define(version: 20151016172957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20151016171627) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
   end
 
 end
